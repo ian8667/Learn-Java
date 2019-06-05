835fef2c436257ffb09019165fcf047ffce3f079
Ian <ian3924@yahoo.co.uk>
2019-06-05 20:13:35 +0100

    stream() and Arrays.asList
    Practising stream output with the statement:
    mylist.stream().forEach((x) -> {System.out.println(x); } );

---
+++ coreJava.java
@@ -0,0 +1,86 @@
+// Copyright (c) 2002 MyHouse
+//package ian;
+import java.util.*;
+// Hadoop imports
+/**
+ * <p>A file to practice my Java as I go through the book
+ * 'Core Java Volume 2 - Advanced Features'.</p>
+ * <p><i>import</i> statements <b>must</b> go above this comment.</p>
+ *
+ * <p>Testing a Swing Look and Feel - see https://laffy.dev.java.net/</p>
+ *
+ * <p>Sets the frame to the centre of the screen:<br />
+ *   <JFrame>.setLocationRelativeTo(null);</p>
+ *
+ * <p><pre>Another useful 'main' method.
+ *   public static void main(String[] args) {
+ *     gmt var1 = new gmt();
+ *     //
+ *     var1.getTimeInfo();
+ *     System.out.println("\nEnd of run for gmt");
+ *     System.exit(0);
+ *   }//end of main</pre></p>
+ *
+ * <p>1 millisecond=0.001 second. ie 5000 milliseconds=5 seconds.</p>
+ *
+ * <p>javac -Xlint:all coreJava.java</p>
+ *
+ * <p>enum Season { WINTER, SPRING, SUMMER, FALL }<br />
+ * http://java.sun.com/j2se/1.5.0/docs/guide/language/enums.html</p>
+ *
+ * <p>java -Xrunhprof:help<br />
+ * HPROF: Heap and CPU Profiling Agent (JVMTI Demonstration Code)</p>
+ *
+ * <p>String outputEncoding = "US-ASCII";</p>
+ *
+ * <blockquote>
+ * <pre>
+ *    Some sample text as Sun Microsystems, Inc does it.
+ * </pre>
+ * </blockquote>
+ * <p>
+ * JDK 12 Documentation
+ * https://docs.oracle.com/en/java/javase/12/
+ * https://docs.oracle.com/en/java/javase/12/docs/api/index.html
+ *
+ * @author Ian Molloy April 2001
+ * @version (#)coreJava.java        3.78 2019-06-05
+ */
+public class coreJava {
+private byte dummy;
+  /**
+   * Constructor
+   */
+  public coreJava() {
+    this.dummy = 99;
+    launchFrame();
+  }//end of constructor
+
+  /**
+   * Working test method.
+   * Floating point formatting to decimal places: %.2f
+   * A format of %03d will pad, for example, a 5 to 005.
+   * topLevel.setLocationRelativeTo(null);
+   */
+  public void launchFrame() {
+    System.out.printf("Start of test on %tc%n", new java.util.Date());
+    // ---------------------------------------------------------------
+List<Integer> mylist = Arrays.asList(2,4,6,8,10,12,14,16);
+mylist.stream().forEach((x) -> {System.out.println(x); } );
+    // ---------------------------------------------------------------
+    System.out.printf("End of test on %tc%n", new java.util.Date());
+  }//end of launchFrame
+
+  /**
+   * main
+   * @param args
+   */
+  public static void main(String[] args) {
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+       public void run() {
+         new coreJava();
+       }
+    });
+  }//end of main
+
+}//end of class
